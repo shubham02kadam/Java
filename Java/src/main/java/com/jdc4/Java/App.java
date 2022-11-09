b4a9b3f43854827453026fe8dbfa9db62bbf90d2
shital <shubham02kadam@gmail.com>
2022-11-09 19:33:58 +0530

    First commit

---
+++ Java/src/main/java/com/jdc4/Java/App.java
@@ -0,0 +1,13 @@
+package com.jdc4.Java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
